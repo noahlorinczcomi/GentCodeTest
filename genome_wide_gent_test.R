7835ec4644f6ed813a3249172a4845f37620df99
Noah Lorincz-Comi <39975632+noahlorinczcomi@users.noreply.github.com>
2025-08-23 23:15:05 -0400

    Update genome_wide_gent_test.R

--- genome_wide_gent_test.R
+++ genome_wide_gent_test.R
@@ -21,10 +21,13 @@ ad_result=gent_genomewide(
     effect_allele='Effect_allele', # column name of SNP effect allele in ad_gwas
     z_statistic='z',               # Z-statistic column is not present in ad_gwas so set NULL
     verbose=TRUE)                  # TRUE indicates that progress should be printed to the console
+saveRDS(ad_result,'ad_result.Rds')
 
 # Plot summaries
 #p1=gent_manhattan(ad_result) + coord_cartesian(ylim=c(0,100))
 #p2=gent_qq(ad_result) + ggtitle('QQ plot of GenT (AD)')
+#ggsave('gent_manhattan.pdf',p1,width=6,height=4)
+#ggsave('gent_qq.pdf',p2,width=4,height=4)
 
 # Fine-mapping
 finemapping_results_clumping=gent_finemap(
@@ -36,6 +39,7 @@ finemapping_results_clumping=gent_finemap(
     clump_r2=0.01,                  # genes whose squared correlation is below this threshold are considered statistically independent
     window_kb_width=2000,           # size of locus windows (kilobases) in which gene-level fine-mapping will be performed
     verbose=TRUE)                   # if TRUE, messages will be printed to the console
+saveRDS(finemapping_results_clumping,'finemapping_results_clumping.Rds')
 
 finemapping_results_setgenes=gent_finemap(
     gent_results=ad_result,                             # output of gent_genomewide(), mugent_genomewide(), or xgent_genomewide()
@@ -44,11 +48,4 @@ finemapping_results_setgenes=gent_finemap(
     index_genes=c('BIN1','APOE','PCSK9'),               # genes indexing test loci
     window_kb_width=2000,                               # size of locus windows (kilobases) in which gene-level fine-mapping will be performed
     verbose=TRUE)                                       # if TRUE, messages will be printed to the console
-
-# save data and plots
-saveRDS(ad_result,'ad_result.Rds')
-saveRDS(finemapping_results_clumping,'finemapping_results_clumping.Rds')
 saveRDS(finemapping_results_setgenes,'finemapping_results_setgenes.Rds')
-#ggsave('gent_manhattan.pdf',p1,width=6,height=4)
-#ggsave('gent_qq.pdf',p2,width=4,height=4)
-
